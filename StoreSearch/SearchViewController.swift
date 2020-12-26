062f89faa887979c7bfbbc84874852ab565b54da
Wesley Osborne <wesleyosborne1@gmail.com>
2020-12-26 17:24:34 -0500

    Update error handling for network requests

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -14,6 +14,7 @@ class SearchViewController: UIViewController {
   var searchResults = [SearchResult]()
   var hasSearched = false
   var isLoading = false
+  var dataTask: URLSessionDataTask?
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -74,6 +75,7 @@ extension SearchViewController: UISearchBarDelegate {
     if !searchBar.text!.isEmpty {
       searchBar.resignFirstResponder()
       
+      dataTask?.cancel()
       isLoading = true
       tableView.reloadData()
       
@@ -82,9 +84,9 @@ extension SearchViewController: UISearchBarDelegate {
       
       let url = iTunesUrl(searchText: searchBar.text!)
       let session = URLSession.shared
-      let dataTask = session.dataTask(with: url) {data, response, error in
-        if let error = error {
-          print("Failure! \(error.localizedDescription)")
+      dataTask = session.dataTask(with: url) {data, response, error in
+        if let error = error as NSError?, error.code == -999 {
+          return
         } else if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
           if let data = data {
             self.searchResults = self.parse(data: data)
@@ -106,7 +108,7 @@ extension SearchViewController: UISearchBarDelegate {
           self.showNetworkError()
         }
       }
-      dataTask.resume()
+      dataTask?.resume()
     }
   }
   
