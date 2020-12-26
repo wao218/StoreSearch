56b2dedb82d4075a4386ef3495af552120cde3f7
Wesley Osborne <wesleyosborne1@gmail.com>
2020-12-26 18:32:05 -0500

    Add capability to download images from the iTunes server

---
+++ StoreSearch/UIImageView+DownloadImage.swift
@@ -0,0 +1,29 @@
+//
+//  UIImageView+DownloadImage.swift
+//  StoreSearch
+//
+//  Created by Wesley Osborne on 12/26/20.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+  func loadImage(url: URL) -> URLSessionDownloadTask {
+    let session = URLSession.shared
+    
+    let downloadTask = session.downloadTask(with: url) {
+      [weak self] url, _, error in
+      
+      if error == nil, let url = url, let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+        DispatchQueue.main.async {
+          if let weakSelf = self {
+            weakSelf.image = image
+          }
+        }
+      }
+    }
+    downloadTask.resume()
+    return downloadTask
+  }
+}
