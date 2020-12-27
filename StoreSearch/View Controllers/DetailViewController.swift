5a175888b6041e25442a9f59346e34e16d42f882
Wesley Osborne <wesleyosborne1@gmail.com>
2020-12-27 15:46:23 -0500

    Add animations to Pop-up View for transition in and out

--- StoreSearch/View Controllers/DetailViewController.swift
+++ StoreSearch/View Controllers/DetailViewController.swift
@@ -30,6 +30,17 @@ class DetailViewController: UIViewController {
     if searchResult != nil {
       updateUI()
     }
+    
+    // Gradient View
+    view.backgroundColor = UIColor.clear
+    let dimmingView = GradientView(frame: CGRect.zero)
+    dimmingView.frame = view.bounds
+    view.insertSubview(dimmingView, at: 0)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    transitioningDelegate = self
   }
   
   // MARK: - Actions
@@ -86,5 +97,15 @@ class DetailViewController: UIViewController {
 extension DetailViewController: UIGestureRecognizerDelegate {
   func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
     return (touch.view === self.view)
+  }
+}
+
+extension DetailViewController: UIViewControllerTransitioningDelegate {
+  func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+      return BounceAnimationController()
+  }
+  
+  func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    return SlideOutAnimationController()
   }
 }
